e54d327a4f51ec42eb8173fd21bc0343e9c5fa93
Sergey Emelyanov <karagabul@gmail.com>
2021-01-28 16:36:48 +0300

    add user and set locale

--- bash/install_postgres.sh
+++ bash/install_postgres.sh
@@ -8,6 +8,14 @@ echo "deb http://apt.postgresql.org/pub/repos/apt $release-pgdg main"  > tmp_deb
 rm tmp_deb
 
 command=$(echo "
+    sudo locale-gen ru_RU
+    && sudo locale-gen ru_RU.UTF-8
+    && sudo update-locale LANG=ru_RU.UTF-8
+")
+./ssh.sh DB $command
+./ssh.sh DB "locale"
+
+command=$(echo "
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - 
     && sudo apt-get update 
     && sudo apt-get install -y libicu-dev
@@ -16,5 +24,8 @@ command=$(echo "
     && sudo service postgresql start 
     && sudo service postgresql status"
 )
+./ssh.sh DB $command
 
+sql=$(echo "ALTER USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';")
+command=$(echo "sudo -i -u $DB_USER psql -c "'"'$sql'"'"")
 ./ssh.sh DB $command
